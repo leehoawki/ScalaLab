bfafb1613d744d36d3cf0b7f9bb90df988ccafc3
leehoawki@gmail.com <kisshot0612>
2017-04-20 22:14:39 +0800

    ch5 init

--- src/main/scala/ch05/MyStream.scala
+++ src/main/scala/ch05/MyStream.scala
@@ -1,45 +1,63 @@
 package ch05
 
-import ch03.MyList
+import ch03.{MyCons, MyList, MyNil}
 
 sealed trait MyStream[+A] {
-  def toList: MyList[A]
 
   def take(n: Int): MyStream[A]
 
   def drop(n: Int): MyStream[A]
 
   def takeWhile(p: A => Boolean): MyStream[A]
+
+  def foldRight[B](z: => B)(f: (A, => B) => B): B = this match {
+    case MyScons(h, t) => f(h(), t().foldRight(z)(f))
+    case _ => z
+  }
+
+  def toList: MyList[A] = foldRight(MyNil: MyList[A])((a, b) => MyCons(a, b))
+
+  def exists(p: A => Boolean): Boolean = foldRight(false)((a, b) => p(a) || b)
+
+  def forAll(p: A => Boolean): Boolean = foldRight(true)((a, b) => p(a) && b)
+
+  def takeWhile2(p: A => Boolean) = foldRight(MyEmpty: MyStream[A])((a, b) => if (p(a)) MyStream.scons(a, b) else MyEmpty)
+
+  def headOption: Option[A] = ???
+
+  def map[B](f: A => B): MyStream[B] = foldRight(MyEmpty: MyStream[B])((a, b) => MyStream.scons(f(a), b))
+
+  def filter(p: A => Boolean): MyStream[A] = foldRight(MyEmpty: MyStream[A])((a, b) => if (p(a)) MyStream.scons(a, b) else b)
+
+  def flatMap[B](f: A => MyStream[B]): MyStream[B] = foldRight(MyEmpty: MyStream[B])((a, b) => f(a).append(b))
+
+  def append[B >: A](b: => MyStream[B]): MyStream[B] = ???
 }
 
 case object MyEmpty extends MyStream[Nothing] {
-  override def toList: MyList[Nothing] = ???
+  override def take(n: Int): MyStream[Nothing] = this
 
-  override def take(n: Int): MyStream[Nothing] = ???
+  override def drop(n: Int): MyStream[Nothing] = this
 
-  override def drop(n: Int): MyStream[Nothing] = ???
-
-  override def takeWhile(p: (Nothing) => Boolean): MyStream[Nothing] = ???
+  override def takeWhile(p: (Nothing) => Boolean): MyStream[Nothing] = this
 }
 
-case class MyCons[+A](h: () => A, t: () => MyStream[A]) extends MyStream[A] {
-  override def toList: MyList[A] = ???
+case class MyScons[+A](h: () => A, t: () => MyStream[A]) extends MyStream[A] {
+  override def take(n: Int): MyStream[A] = if (n > 0) MyScons(h, () => t().take(n - 1)) else MyEmpty
 
-  override def take(n: Int): MyStream[A] = ???
+  override def drop(n: Int): MyStream[A] = if (n <= 0) MyScons(h, () => t()) else t().drop(n - 1)
 
-  override def drop(n: Int): MyStream[A] = ???
-
-  override def takeWhile(p: (A) => Boolean): MyStream[A] = ???
+  override def takeWhile(p: (A) => Boolean): MyStream[A] = if (p(h())) MyScons(h, () => t().takeWhile(p)) else MyEmpty
 }
 
 object MyStream {
-  def cons[A](hd: => A, tl: => MyStream[A]): MyStream[A] = {
+  def scons[A](hd: => A, tl: => MyStream[A]): MyStream[A] = {
     lazy val head = hd
     lazy val tail = tl
-    MyCons(() => head, () => tail)
+    MyScons(() => head, () => tail)
   }
 
   def empty[A]: MyStream[A] = MyEmpty
 
-  def apply[A](as: A*): MyStream[A] = if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+  def apply[A](as: A*): MyStream[A] = if (as.isEmpty) empty else scons(as.head, apply(as.tail: _*))
 }
--- src/test/scala/ch05/MyStreamSpec.scala
+++ src/test/scala/ch05/MyStreamSpec.scala
@@ -1,7 +1,66 @@
 package ch05
 
+import ch03.{MyList, MyNil}
+import ch04.{MyNone, MySome}
 import org.scalatest.{FlatSpec, Matchers}
 
 class MyStreamSpec extends FlatSpec with Matchers {
+  "ToList" should "be correct" in {
+    MyStream(1, 2, 3).toList shouldEqual MyList(1, 2, 3)
+    MyStream().toList shouldEqual MyNil
+    MyEmpty.toList shouldEqual MyNil
+  }
 
+  "Take" should "be correct" in {
+    MyStream(1, 2, 3).take(-1).toList shouldEqual MyNil
+    MyStream(1, 2, 3).take(0).toList shouldEqual MyNil
+    MyStream(1, 2, 3).take(1).toList shouldEqual MyList(1)
+    MyStream(1, 2, 3).take(2).toList shouldEqual MyList(1, 2)
+    MyStream(1, 2, 3).take(3).toList shouldEqual MyList(1, 2, 3)
+    MyStream(1, 2, 3).take(4).toList shouldEqual MyList(1, 2, 3)
+    MyStream().take(0).toList shouldEqual MyNil
+    MyStream().take(1).toList shouldEqual MyNil
+  }
+
+  "Drop" should "be correct" in {
+    MyStream(1, 2, 3).drop(-1).toList shouldEqual MyList(1, 2, 3)
+    MyStream(1, 2, 3).drop(0).toList shouldEqual MyList(1, 2, 3)
+    MyStream(1, 2, 3).drop(1).toList shouldEqual MyList(2, 3)
+    MyStream(1, 2, 3).drop(2).toList shouldEqual MyList(3)
+    MyStream(1, 2, 3).drop(3).toList shouldEqual MyNil
+    MyStream(1, 2, 3).drop(4).toList shouldEqual MyNil
+    MyStream().drop(0).toList shouldEqual MyNil
+    MyStream().drop(1).toList shouldEqual MyNil
+  }
+
+  "Takewhile" should "be correct" in {
+    MyStream(1, 2, 3, 2, 1).takeWhile(_ < 0).toList shouldEqual MyNil
+    MyStream(1, 2, 3, 2, 1).takeWhile(_ > 0).toList shouldEqual MyList(1, 2, 3, 2, 1)
+    MyStream(1, 2, 3, 2, 1).takeWhile(_ > 2).toList shouldEqual MyNil
+    MyStream(1, 2, 3, 2, 1).takeWhile(_ < 2).toList shouldEqual MyList(1)
+    MyStream(1, 2, 3, 2, 1).takeWhile(_ < 3).toList shouldEqual MyList(1, 2)
+    MyStream().takeWhile(_ => true).toList shouldEqual MyNil
+    MyStream().takeWhile(_ => false).toList shouldEqual MyNil
+
+    MyStream(1, 2, 3, 2, 1).takeWhile2(_ < 0).toList shouldEqual MyNil
+    MyStream(1, 2, 3, 2, 1).takeWhile2(_ > 0).toList shouldEqual MyList(1, 2, 3, 2, 1)
+    MyStream(1, 2, 3, 2, 1).takeWhile2(_ > 2).toList shouldEqual MyNil
+    MyStream(1, 2, 3, 2, 1).takeWhile2(_ < 2).toList shouldEqual MyList(1)
+    MyStream(1, 2, 3, 2, 1).takeWhile2(_ < 3).toList shouldEqual MyList(1, 2)
+
+    MyStream().takeWhile2(_ => true).toList shouldEqual MyNil
+    MyStream().takeWhile2(_ => false).toList shouldEqual MyNil
+  }
+
+  "HeadOption" should "be correct" in {
+    MyStream(1, 2, 3).headOption shouldEqual MySome(1)
+    MyStream().headOption shouldEqual MyNone
+  }
+
+  "Append" should "be correct" in {
+    MyEmpty.append(MyEmpty) shouldEqual MyEmpty
+    MyEmpty.append(MyStream(1, 2, 3)) shouldEqual MyStream(1, 2, 3)
+    MyStream(1, 2, 3).append(MyEmpty) shouldEqual MyStream(1, 2, 3)
+    MyStream(1, 2, 3).append(MyStream(1, 2, 3)) shouldEqual MyStream(1, 2, 3, 1, 2, 3)
+  }
 }
