eeae57ef0c0d094689f9bac15ae777ab366c14e0
leehoawki@gmail.com <kisshot0612>
2017-04-27 23:47:40 +0800

    ch6 update

--- src/test/scala/ch06/MyStateSpec.scala
+++ src/test/scala/ch06/MyStateSpec.scala
@@ -1,7 +1,17 @@
 package ch06
 
+import ch03.MyList
+import ch05.MyStream
 import org.scalatest.{FlatSpec, Matchers}
 
 class MyStateSpec extends FlatSpec with Matchers {
-  "Sequence" should "be correct" in ???
+  "MyState" should "be correct" in {
+    type NewRand[A] = MyState[MyRNG, A]
+
+    def int: NewRand[Int] = MyState(rng => rng.nextInt)
+
+    def ints(count: Int): NewRand[MyList[Int]] = MyState.sequence(MyStream.constant(int).take(count).toList)
+
+    int.flatMap(x => int.flatMap(y => ints(x % 10).map(xs => MyList.map(xs)(_ % y))))
+  }
 }
